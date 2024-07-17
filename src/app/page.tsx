83349873c936a40c9f7f071db9769efead2bb29d
tranvandat05032002 <tranvandathtml0503@gmail.com>
2024-07-17 15:45:32 +0700

    feat: validator and envconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
-import envClientConfig from "../../configClient";
-console.log(envClientConfig)
+import envClientConfig from "../configClient";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
