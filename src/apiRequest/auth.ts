396144048bd3cdb350cc52b9d280fe520442a211
tranvandat05032002 <tranvandathtml0503@gmail.com>
2024-07-23 16:50:40 +0700

    feat: router handler logout

--- src/apiRequest/auth.ts
+++ src/apiRequest/auth.ts
@@ -1,12 +1,26 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import { LoginBodyType, LoginResType, LogoutBodyType } from "@/schemaValidations/auth.schema";
 const endpoint = {
     serverLoginEndpoint: '/auth/login',
-    loginEndPoint: '/api/auth/login'
+    loginEndPoint: '/api/auth/login',
+    serverLogoutEndpoint: '/auth/logout',
+    logoutEndpoint: '/api/auth/logout'
 }
 const authApiRequest = {
     serverLogin: (body: LoginBodyType) => http.post<LoginResType>(endpoint.serverLoginEndpoint, body),
     login: (body: LoginBodyType) => http.post<LoginResType>(endpoint.loginEndPoint, body, {
+        baseUrl: ''
+    }),
+    sLogout: (body: LogoutBodyType & {
+        accessToken: string
+    }) => http.post(endpoint.serverLogoutEndpoint, {
+        refreshToken: body.refreshToken
+    }, {
+        headers: {
+            Authorization: `Bearer ${body.accessToken}`
+        }
+    }),
+    logout: () => http.post(endpoint.logoutEndpoint, null, {
         baseUrl: ''
     })
 }
