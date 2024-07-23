38497614cca0184d90ff0e8d0a0cc257709ea602
tranvandat05032002 <tranvandathtml0503@gmail.com>
2024-07-23 08:55:34 +0700

    feat: Template UI login

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
-import autoCert from "anchor-pki/auto-cert/integrations/next";
+// import autoCert from "anchor-pki/auto-cert/integrations/next";
 
-const withAutoCert = autoCert({
-  enabledEnv: "development",
-});
+// const withAutoCert = autoCert({
+//   enabledEnv: "development",
+// });
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withAutoCert(nextConfig);
+// export default withAutoCert(nextConfig);
+export default nextConfig;
---
+++ src/app/(public)/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./login-form";
+
+
+export default function Login() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <LoginForm />
+    </div>
+  )
+}
