f36f36742c1534c99d0d73333037b40053e4893a
dattv <tranvandatevondev0503@gmail.com>
2024-08-11 21:03:22 +0700

    feat: Custom QRCode Canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -12,18 +12,50 @@ export default function QRCodeTable({
     width?: number
 }) {
     const canvasRef = React.useRef<HTMLCanvasElement>(null)
+    // Hiện tại: Thư viện QRCode nó sẽ vẽ lên cái thẻ canvas
+    // Bây giờ: Sẽ tạo ra một acis thẻ acnvas ảo để thư viện QRCode nó vẽ QR lên trên đó.
+    // Và sau đó sẽ edit trên thẻ canvas thật
+    // Cuối cùng thì sẽ đưa ra cái thẻ canvas ảo chứa QR Code ở trên vào thẻ Canvas thật
     React.useEffect(() => {
-        const QRCode = require('qrcode')
-        const canvas = canvasRef.current
+        const canvas = canvasRef.current!
+        canvas.width = width
+        canvas.height = width + 70
 
-        QRCode.toCanvas(canvas, getTableLink({
+        const canvasContext = canvas.getContext('2d')!
+        // Tạo khung QR
+        canvasContext.fillStyle = "#fff"
+        canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+        // style
+        canvasContext.font = 'bold 20px Arial';
+        canvasContext.textAlign = 'center';
+        canvasContext.fillStyle = '#000';
+        canvasContext.fillText('Meteor'.toUpperCase(), canvas.width / 2, 30);
+        canvasContext.font = '20px Arial'
+        canvasContext.fillText(`Bàn số ${tableNumber}`, canvas.width / 2, canvas.width + 20)
+        canvasContext.fillText(`Quét mã QR để gọi món`, canvas.width / 2, canvas.width + 45)
+
+        const QRCode = require('qrcode')
+
+        const virtualCanvas = document.createElement('canvas')
+        QRCode.toCanvas(virtualCanvas, getTableLink({
             token,
             tableNumber
-        }), function (error: any) {
+        }), {
+            width,
+            margin: 2,
+            color: {
+                dark: '#87cbfb',  // Mã QR màu xanh
+                light: '#FFFFFF'  // Nền trắng (hoặc bạn có thể đặt màu nền khác)
+            }
+        }, function (error: any) {
             if (error) console.error(error)
             console.log('success!');
+            const canvasWidth = Number(width) - 35 as number
+            const qrCodeX = (width - canvasWidth) / 2
+            canvasContext.drawImage(virtualCanvas, qrCodeX, 35, canvasWidth, canvasWidth)
         })
-    })
+    }, [token, tableNumber, width])
+
     return (
         <canvas ref={canvasRef}></canvas>
     )
