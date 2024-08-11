43046951d61f0d38dc017e0fb8b8e9494c55cb93
dattv <tranvandatevondev0503@gmail.com>
2024-08-11 13:59:41 +0700

    feat: Add Table

--- src/app/manage/tables/add-table.tsx
+++ src/app/manage/tables/add-table.tsx
@@ -8,13 +8,18 @@ import { PlusCircle } from 'lucide-react'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
-import { getVietnameseTableStatus } from '@/lib/utils'
+import { getVietnameseTableStatus, handleErrorApi } from '@/lib/utils'
 import { CreateTableBody, CreateTableBodyType } from '@/schemaValidations/table.schema'
 import { TableStatus, TableStatusValues } from '@/constants/type'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { useAddTableMutation } from '@/queries/useTable'
+import NProgress from 'nprogress'
+import { toast } from '@/components/ui/use-toast'
+
 
 export default function AddTable() {
   const [open, setOpen] = useState(false)
+  const addTableMutation = useAddTableMutation()
   const form = useForm<CreateTableBodyType>({
     resolver: zodResolver(CreateTableBody),
     defaultValues: {
@@ -23,8 +28,39 @@ export default function AddTable() {
       status: TableStatus.Hidden
     }
   })
+  const reset = () => {
+    setOpen(false)
+  }
+  const onSubmit = async (values: CreateTableBodyType) => {
+    NProgress.start()
+    if (addTableMutation.isPending) return;
+    try {
+      const result = await addTableMutation.mutateAsync(values)
+      toast({
+        description: result.payload.message
+      })
+      reset()
+      setOpen(false)
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+    finally {
+      NProgress.done()
+      NProgress.remove()
+    }
+  }
   return (
-    <Dialog onOpenChange={setOpen} open={open}>
+    <Dialog onOpenChange={(value) => {
+      if (!value) {
+        reset()
+      }
+      setOpen(
+        value
+      )
+    }} open={open}>
       <DialogTrigger asChild>
         <Button size='sm' className='h-7 gap-1'>
           <PlusCircle className='h-3.5 w-3.5' />
@@ -36,7 +72,7 @@ export default function AddTable() {
           <DialogTitle>Thêm bàn</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='add-table-form'>
+          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='add-table-form' onSubmit={form.handleSubmit(onSubmit)}>
             <div className='grid gap-4 py-4'>
               <FormField
                 control={form.control}
@@ -101,6 +137,7 @@ export default function AddTable() {
           </form>
         </Form>
         <DialogFooter>
+          {/* Thêm id của form để có thể bắt được sự kiện obsubmit ở bên ngoài */}
           <Button type='submit' form='add-table-form'>
             Thêm
           </Button>
